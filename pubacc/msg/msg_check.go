b4f07b217ad082184bba619b984c49f548f112e5
Kenshin <xiyingit@gmail.com>
2015-01-17 15:36:29 +0800

    ...

---
+++ pubacc/msg/msg_check.go
@@ -0,0 +1,21 @@
+package msg
+
+import (
+    "sort"
+    "crypto/sha1"
+)
+
+type RawQuerySt struct {
+    Signature string
+    Timestamp string
+    Nonce     string
+    Echostr   string
+}
+
+func (this *RawQuerySt) MsgCheck(token string) bool {
+    strarr := []string{token, this.Timestamp, this.Nonce}
+    sort.Strings(strarr)
+    signature := strarr[0] + strarr[1] + strarr[2]
+    signature = string(sha1.Sum([]byte(signature)))
+    return signature == this.Signature
+}
---
+++ pubacc/msg/xml_msg_send.go
@@ -0,0 +1,101 @@
+package msg
+
+import "encoding/xml"
+import "github.com/udoyu/webchat/pubacc/tools"
+
+type XmlMsgS struct {
+    XMLName xml.Name `xml:"xml"`
+    *XmlMsgHeadS
+    *XmlMsgTextS
+    *XmlMsgImageS
+    *XmlMsgVoiceS
+    *XmlMsgVideoS
+    *XmlMsgMusicS
+    *XmlMsgArticleS
+}
+
+type XmlMsgHeadS XmlMsgHead
+type XmlMsgTextS XmlMsgText
+type XmlMsgImages struct {
+    Image *XmlMsgMedia
+}
+type XmlMsgVoiceS struct {
+    voice *XmlMsgMedia
+}
+type XmlMsgVideoS struct {
+    video *XmlMsgMedia
+}
+type XmlMsgMusicS struct {
+    Music *XmlMsgMusic
+}
+type XmlMsgArticleS struct {
+    ArticleCount *int64
+    Articles *XmlMsgArticleItemsS
+}
+
+type XmlMsgMusic struct {
+    Title *string
+    Description *string
+    MusicUrl *string
+    HQMusicUrl *string
+    ThumbMediaId *string
+}
+
+type XmlMsgArticleItemsS struct {
+    []Item *XmlMsgArtItemS `xml:"item`
+}
+
+type XmlMsgArtItemS struct {
+    Title *string
+    Description *string
+    PicUrl *string
+    Url *string
+}
+
+//回复文本消息
+func RespText(head *XmlMsgHeadS, content *XmlMsgTextS) (string,error) {
+    head.MsgType = tools.String(TEXT)
+    msgS := &XmlMsgS{XmlMsgHeadS:head, XmlMsgTextS:content}
+    data, err := tools.Marshal(msgS)
+    return string(data), err
+}
+
+//回复图片消息
+func RespImage(head *XmlMsgHeadS, image *XmlMsgImageS) (string, error) {
+    head.MsgType = tools.String(IMAGE)
+    msgS := &XmlMsgS{XmlMsgHeadS:head, XmlMsgImageS:image}
+    data, err := tools.Marshal(msgS)
+    return string(data), err
+}
+
+//回复语音消息
+func RespVoice(head *XmlMsgHeadS, voice *XmlMsgVoiceS) (string, error) {
+    head.MsgType = tools.String(IMAGE)
+    msgS := &XmlMsgS{XmlMsgHeadS:head, XmlMsgVoiceS:voice}
+    data, err := tools.Marshal(msgS)
+    return string(data), err
+}
+
+//回复视频消息
+func RespVideo(head *XmlMsgHeadS, video *XmlMsgVideoS) (string, error) {
+    head.MsgType = tools.String(IMAGE)
+    msgS := &XmlMsgS{XmlMsgHeadS:head, XmlMsgVideoS:video}
+    data, err := tools.Marshal(msgS)
+    return string(data), err
+}
+
+//回复音乐消息
+func RespMusic(head *XmlMsgHeadS, music *XmlMsgMusicS) (string, error) {
+    head.MsgType = tools.String(MUSIC)
+    msgS := &XmlMsgS{XmlMsgHeadS:head, XmlMsgMusicS:music}
+    data, err := tools.Marshal(msgS)
+    return string(data), err
+}
+
+//回复图文消息
+func RespNews(head *XmlMsgHeadS, artcile *XmlMsgArticleS) (string, error) {
+    head.MsgType = tools.String(NEWS)
+    msgS := &XmlMsgS{XmlMsgHeadS:head, XmlMsgArticleS:artcile}
+    data, err := tools.Marshal(msgS)
+    return string(data), err
+}
